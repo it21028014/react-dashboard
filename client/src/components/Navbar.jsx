5398be604ee43a222406e90e12f91afd57ccaf14
sansika <it21028014@my.sliit.lk>
2023-12-08 17:11:20 +0530

    Done up this

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   LightModeOutlined,
   DarkModeOutlined,
@@ -8,28 +8,66 @@ import {
   ArrowDropDownCircleOutlined,
   Input,
 } from "@mui/icons-material";
+import {
+  AppBar,
+  Button,
+  Box,
+  Typography,
+  IconButton,
+  InputBase,
+  Toolbar,
+  Menu,
+  MenuItem,
+} from "@mui/material";
 import FlexBetween from "./FlexBetween";
 import { useDispatch } from "react-redux";
 import { setMode } from "state";
 import profileImage from "assests/profile.jpeg";
 import { useTheme } from "@emotion/react";
+import { createSlice } from "@reduxjs/toolkit";
 
 const Navbar = () => {
   const dispatch = useDispatch();
   const theme = useTheme();
 
   return (
-    <AppBar sx={{ position: "static", background: "none", boxShadow: "none" }}>
+    <AppBar
+      sx={{
+        position: "static",
+        background: "none",
+        boxShadow: "none",
+      }}
+    >
       <Toolbar sx={{ justifyContent: "space-between" }}>
-        <FlexBetween
-          backgroundColor={theme.palette.background.alt}
-          borderRadius="9px"
-          gap="3rem"
-          p="0.1rem 1.5rem"
-        >
-          <InputBase placeholder="Search..." />
+        {/* LEFT SIDE */}
+        <FlexBetween>
+          <IconButton onClick={() => console.log("open/close sidebar")}>
+            <MenuIcon />
+          </IconButton>
+          <FlexBetween
+            backgroundColor={theme.palette.background.alt}
+            borderRadius="9px"
+            gap="3rem"
+            p="0.1 rem 1.5rem"
+          >
+            <InputBase placeholder="Search..." />
+            <IconButton>
+              <Search />
+            </IconButton>
+          </FlexBetween>
+        </FlexBetween>
+
+        {/* RIGHT SIDE */}
+        <FlexBetween gap="1.5rem">
+          <IconButton onClick={() => dispatch(setMode())}>
+            {theme.palette.mode === "dark" ? (
+              <DarkModeOutlined sx={{ fontSize: "25px" }} />
+            ) : (
+              <LightModeOutlined sx={{ fontSize: "25px" }} />
+            )}
+          </IconButton>
           <IconButton>
-            <Search />
+            <SettingsOutlined sx={{ fontSize: "25px" }} />
           </IconButton>
         </FlexBetween>
       </Toolbar>
